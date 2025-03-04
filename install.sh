9e4f5eb318d0bd46c8c76cbfc37646b6ecc242cd
little kid <133933012+LittleKidd0@users.noreply.github.com>
2025-03-04 00:44:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,31 @@
-sudo apt-get install jq
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root (sudo)." >&2
+    exit 1
+fi
+
+if [ -f "/etc/debian_version" ]; then
+    PKG_MANAGER="apt-get"
+elif [ -d "/data/data/com.termux/files/usr" ]; then
+    PKG_MANAGER="pkg"
+else
+    echo "Unsupported system." >&2
+    exit 1
+fi
+
+$PKG_MANAGER update -y && $PKG_MANAGER install -y jq nodejs npm python3-pip
 
 if [ ! -f package.json ]; then
-  echo "package.json not found. Creating one..."
-  npm init -y
+    echo "package.json not found. Creating one..."
+    npm init -y
 fi
 
 echo "Modifying package.json to use ES modules..."
-
 jq '. + {"type": "module"}' package.json > temp.json && mv temp.json package.json
 
 echo "Installing dependencies..."
 npm install node-fetch axios qs
-
-echo "All set! ES modules configured and dependencies installed."
 
 pip install -r requirements.txt
 python3 main.py
